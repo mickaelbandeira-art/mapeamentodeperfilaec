2b25bc932679d8df94d8d38eef6f496fec29a303
mickaelbandeira-arte <mickael.bandeira@aec.com.br>
2026-01-21 15:39:26 -0300

    commit

--- full_setup_placeholder.sql
+++
@@ -1,8 +0,0 @@
-
--- Full Setup Script for New Project
--- This file concatenates all migrations and seed data.
-
--- 1. Migrations
--- (User should copy-paste content from 20251114... etc or running this file if I can generate it)
--- Since I cannot easily concat files in this tool without reading them all, I will create a node script to do it.
--- This file is just a placeholder for the plan.
